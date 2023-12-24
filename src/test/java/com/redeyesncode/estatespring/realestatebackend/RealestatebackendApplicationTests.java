3f61e14ee58e7abd1dc652457a92f4dcc1f286e0
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2023-12-25 00:08:03 +0530

    Add Initial Spring Project

---
+++ src/test/java/com/redeyesncode/estatespring/realestatebackend/RealestatebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redeyesncode.estatespring.realestatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestatebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
